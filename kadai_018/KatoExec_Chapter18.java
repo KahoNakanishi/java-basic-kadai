184869b802c9a47d11b66565834e2888412c8f7e
KahoNakanishi <kahomaru0621@gmail.com>
2025-12-06 21:23:33 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,18 @@
+public class KatoExec_Chapter18{
+    public static void main(String[] args){
+        //加藤太郎さんの自己紹介
+        KatoTaro_Chapter18 katotaro = new KatoTaro_Chapter18();
+        katotaro.setGivenName();
+        katotaro.execIntroduce();
+
+        //加藤一郎さん
+        KatoIchiro_Chapter18 katoIchiro = new KatoIchiro_Chapter18();
+        katoIchiro.setGivenName();
+        katoIchiro.execIntroduce();
+
+
+        //加藤花子さん　
+　　　　KatoIchiro_Chapter18 katoIchiro = new KatoIchiro_Chapter18();
+        katoIchiro.setGivenName();
+        katoIchiro.execIntroduce();
+    }
---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,18 @@
+package java.kadai_018_02;
+
+public abstract class Kato_Chapter18{
+    public String familyName = "加藤";
+    public String givenName;
+    public String address = "住所は東京都中野区〇×です";
+
+    public void commonIntroduce(){
+        System.out.println("名前は" + this.familyName + this.givenName + "です");
+        System.out.println(this.address);
+    }
+
+    abstract public void eachIntroduce();
+
+    public void execIntroduce(){
+    }
+
+}
